3b80ce2fbee7f811663f75027465d4876c493766
yinheng <herman.heng.yin@gmail.com>
2019-08-19 23:45:49 +0800

    二分查找 查找第一个等于给定值的元素

--- binarysearch.js
+++ binarysearch.js
@@ -14,5 +14,25 @@ function binarySearch(array, value) {
     return -1;
 }
 
-const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(binarySearch(array, 10));
+function binarySearchFirst(array, value) {
+    let left = 0;
+    let right = array.length - 1;
+    while (left<= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (array[mid] > value) {
+            right = mid - 1;
+        } else if (array[mid] < value) {
+            left = mid + 1;
+        } else {
+            if (mid === 0 || array[mid - 1] != value) {
+                return mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+const array = [0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10];
+console.log(binarySearchFirst(array, 4));
