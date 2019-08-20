e57bd1141d48d6017b2c875448062d3f32379d38
yinheng <herman.heng.yin@gmail.com>
2019-08-20 22:13:48 +0800

    二分查找 查找第一个大于等于 给定值的元素

--- binarysearch.js
+++ binarysearch.js
@@ -44,7 +44,7 @@ function binarySearchLast(array, value) {//二分查找 查找最后一个等于
         } else if (array[mid] < value) {
             left = mid + 1;
         } else {
-            if (mid === array.length - 1 || (array[mid + 1] && array[mid + 1] != value)) {
+            if (mid === array.length - 1 || array[mid + 1] != value) {
                 return mid;
             } else {
                 left = mid + 1;
@@ -54,6 +54,26 @@ function binarySearchLast(array, value) {//二分查找 查找最后一个等于
     return -1;
 }
 
+
+function binarySearchFirstOne(array, value) {//二分查找 查找第一个大于等于 给定值的元素
+    let left = 0;
+    let right = array.length - 1;
+    while (left<= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (array[mid] >= value) {
+            if (mid === 0 || array[mid - 1] < value) {
+                return mid;
+            } else {
+                right = mid - 1;
+            }
+        } else if (array[mid] < value) {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
 const array = [0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10];
 console.log(binarySearchFirst(array, 4));
 console.log(binarySearchLast(array, 4));
+console.log(binarySearchFirstOne(array, 4));
