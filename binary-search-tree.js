7b6064e5f9851c2a0d5819ba297bb73cb6d31fc1
yinheng <herman.heng.yin@gmail.com>
2019-08-27 23:56:08 +0800

    二叉树

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -5,3 +5,26 @@ function Node(value, left, right, parent) {
     this._right = right;
     this._parent = parent;
 }
+
+function BinaryTree() {
+    this.root = null;
+}
+
+BinaryTree.prototype.insert = function (value, current) {
+    if (this.insert === null) {
+        this.root = new Node(value, null, null, null);
+        return;
+    }
+    let insertKey;
+    current = current || this.root;
+    if (current.value > value) {
+        insertKey = "_left";
+    } else {
+        insertKey = "_right";
+    }
+    if (!current[insertKey]) {
+        current[insertKey] = new Node(value, null, null, current);
+    } else {
+        this.insert(value, current[insertKey]);
+    }
+}
