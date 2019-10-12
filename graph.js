e8e5d57faf68e9edcef20dbb4cc2f5fad110594a
yinheng <herman.heng.yin@gmail.com>
2019-10-12 23:52:02 +0800

    修改

--- graph.js
+++ graph.js
@@ -1,3 +1,32 @@
-function Graph() {
+function orderByWeight(input) {
+  const numbers = input.split(' ');
+  numbers.sort();
+  console.log('numbers', numbers);
   
+  const weights = numbers.map(number => {
+    let weight = 0;
+    number.split('').forEach(element => {
+      weight += Number(element);
+    });
+    return weight;
+  });
+  console.log('weights', weights);
+
+  for (let i = 0; i < weights.length; i++) {
+    for (let j = i + 1; j < weights.length; j++) {
+      if (weights[i] > weights[j]) {
+        const tem = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = tem;
+      }
+    }
+  }
+
+  const result= numbers.join(' ');
+  console.log('result', result);
+  
+  return result
 }
+
+
+orderByWeight('65 56 74')
