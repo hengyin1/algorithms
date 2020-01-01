e6ada51be88d332ff42ff4394f1130f57416e4cf
yinheng <herman.heng.yin@gmail.com>
2020-01-01 15:34:11 +0800

    knapsack

--- knapsack.js
+++ knapsack.js
@@ -1,6 +1,6 @@
 function knapsack(weight, n, w) {
   const states = Array(n);
-  for (let i = 0; i < w + 1; i++) {
+  for (let i = 0; i < n; i++) {
     states[i] = Array(w);
   }
 
@@ -39,6 +39,38 @@ function knapsack2(weight, n, w) {
     if (states[i] === true) return i;
   }
   return 0;
+}
+
+function knapsack3(weight, value, n, w) {
+  const states = Array(n);
+  for (let i = 0; i < n; i++) {
+    states[i] = Array(w);
+    for (let j = 0; j < w; j++) {
+      states[i][j] = -1;
+    }
+  }
+
+  states[0][0] = 0;
+  if (weight[0] <= w) states[0][weight[0]] = value[0];
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j <= w; j++) {
+      if (states[i - 1][j] >= 0) states[i][j] = states[i - 1][j];
+    }
+
+    for (let j = 0; j <= w - weight[i]; j++) {
+      if (states[i - 1][j] >= 0) {
+        const v = states[i - 1][j] + value[i];
+        if (v > states[i][j + weight[i]]) states[i][j + weight[i]] = v;
+      }
+    }
+  }
+
+  let maxvalue = -1;
+  for (let i = w; i >= 0; i--) {
+    if (states[n - 1][i] > maxvalue) maxvalue = states[n - 1][i];
+  }
+  return maxvalue;
 }
 
 console.log(knapsack2([2, 2, 4, 6, 3], 5, 9));
